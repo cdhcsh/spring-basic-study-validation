880609590feeac1e81e130908a2d7ac2ca221ec3
cdhcsh <cdhcsh@gmail.com>
2022-12-21 22:06:13 +0900

    12/13 spring-mvc validation/4

--- src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
+++ src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -2,6 +2,9 @@ package hello.itemservice.validation;
 import org.junit.jupiter.api.Test;
 import org.springframework.validation.DefaultMessageCodesResolver;
 import org.springframework.validation.MessageCodesResolver;
+
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 public class MessageCodesResolverTest {
     MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
@@ -9,12 +12,14 @@ public class MessageCodesResolverTest {
     void messageCodesResolverObject() {
         String[] messageCodes = codesResolver.resolveMessageCodes("required",
                 "item");
+        Arrays.stream(messageCodes).forEach(System.out::println);
         assertThat(messageCodes).containsExactly("required.item", "required");
     }
     @Test
     void messageCodesResolverField() {
         String[] messageCodes = codesResolver.resolveMessageCodes("required",
-                "item", "itemName", String.class);
+                "item", "itemName",String.class);
+        Arrays.stream(messageCodes).forEach(System.out::println);
         assertThat(messageCodes).containsExactly(
                 "required.item.itemName",
                 "required.itemName",
